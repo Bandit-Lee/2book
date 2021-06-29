89395afe74fb906ae6c7ffaab2811e7f641dab0f
bandit <2577569090@qq.com>
2021-06-29 21:34:31 +0800

    李东升_导航栏

--- miniprogram/pages/postBook/postBook.js
+++ miniprogram/pages/postBook/postBook.js
@@ -5,47 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    minusStatus: true,
-    courseCount: 1, //设置出售数量相关变量
-    steps:[
-      {
-        text:'设置图书信息'
-      },
-      {
-        text:'填写联系方式'
-      }
-    ],
-    selectorItems:['信息','理学','文法','经管']
+
   },
-  //加减数量
-  addNum: function() {
-    var courseCount = this.data.courseCount;
-    courseCount++;
-    this.setData({
-      courseCount: courseCount,
-      minusStatus: false
-    })
-  },
-  //数字减1
-  minusNum: function() {
-    var courseCount = this.data.courseCount;
-    if (courseCount > 1) {
-      courseCount--;
-    }
-    //数字<=1时，开启 - 按钮的disable状态
-    var minusStatus = courseCount <= 1 ? true : false; 
-    this.setData({
-      courseCount: courseCount,
-      minusStatus: minusStatus
-    });
-  }
-,
-  /*科目选择滚动条,将选择到的显示*/
-  selectorChange:function(e){
-    let i=e.detail.value;
-    let value=this.data.selectorItems[i];
-    this.setData({selector:value});
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
