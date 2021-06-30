890ddc7f03d96e42a98fd93fa0a3578dbea01776
abysswatcher-hqy <2534586898@qq.com>
2021-06-30 21:18:41 +0800

    韩启宇-信息上传完成，界面样式未设置

--- miniprogram/pages/postBook/postBook.js
+++ miniprogram/pages/postBook/postBook.js
@@ -1,15 +1,20 @@
 // pages/postBook/postBook.js
+var utils = require('../postBook/util.js');
 Page({
   data: {
+    connect:'',
+    showImg: '',
+    nowtime: '1',
+    count: 3,
     price: 0,
     active_step: 0,
     fangshi: '默认',
     username: ' ',
     bookname: '默认',
     minusStatus: true,
-    imgURL: '',
+    imgURL: [],
     courseCount: 1, //设置出售数量相关变量
-    imgs: [],//本地图片地址数组
+    imgbox: [], //选择图片
     picPaths: [],//网络路径
     steps: [
       {
@@ -21,6 +26,55 @@ Page({
     ],
     selectorItems: ['计算机', '土木工程', '机电', '林学', '经济管理', '理学']
   },
+//图片上传
+  // 删除照片 &&
+  deleteImg: function (e) {
+    let that = this;
+    let index = e.currentTarget.dataset.deindex;
+    let imgbox = this.data.imgbox;
+    imgbox.splice(index, 1)
+    that.setData({
+      imgbox: imgbox
+    });
+  },
+  // 选择图片 &&&
+  addImage: function (e) {
+    var imgbox = this.data.imgbox;
+    var that = this;
+    var n = 5;
+    if (5 > imgbox.length > 0) {
+      n = 5 - imgbox.length;
+    } else if (imgbox.length == 5) {
+      n = 1;
+    }
+    wx.chooseImage({
+      count: n, // 默认9，设置图片张数
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // console.log(res.tempFilePaths)
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths)
+        if (imgbox.length == 0) {
+          imgbox = tempFilePaths
+        } else if (5 > imgbox.length) {
+          imgbox = imgbox.concat(tempFilePaths);
+        }
+        that.setData({
+          imgbox: imgbox
+        });
+      }
+    })
+  },
+
+   //图片
+   imgbox: function (e) {
+    this.setData({
+      imgbox: e.detail.value
+    })
+  },
+
   //获取填写的信息
   setprice(e){
     this.setData({price: e.detail.value})
@@ -31,14 +85,50 @@ Page({
   setfangshi(e) {
     this.setData({ fangshi: e.detail.value })
   },
+  setconnect(e){
+    this.setData({ connect: e.detail.value })
+  },
   //提交信息
   submit() {
     this.setData({active_step: 1})
     // 云函数入口文件
+    var TIME = utils.formatTime(new Date())
+    this.setData({nowtime:TIME}) 
     const db = wx.cloud.database()
     //获取缓存中的用户名
     var value = wx.getStorageSync('user')
     this.setData({username: value.nickName})
+    //上传图片到云存储
+    console.log('总图片数量：'+this.data.imgbox.length)
+    let promiseArr = [];
+    wx.showLoading({
+      title: '图片上传中',
+      mask: true,
+    })
+    for (let i = 0; i < this.data.imgbox.length; i++) {
+      promiseArr.push(new Promise((reslove, reject) => {
+        let item = this.data.imgbox[i];
+        let suffix = /\.\w+$/.exec(item)[0]; //正则表达式返回文件的扩展名
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
+          filePath: item, // 小程序临时文件路径
+          success: res => {
+            this.setData({
+              fileIDs: this.data.fileIDs.concat(res.fileID),
+            });
+            console.log(this.data.fileIDs)
+            console.log(res.fileID) //输出上传后图片的返回地址
+            reslove();
+          },
+          fail: res => {
+            wx.hideLoading();
+            wx.showToast({
+              title: "上传失败",
+            })
+          }
+        })
+      }));
+    }
     db.collection('fabujilu').add({
       data: {
         //属性:变量名,
@@ -47,8 +137,10 @@ Page({
         getfangshi: this.data.fangshi,
         subject: this.data.selector,
         imgurl: this.data.imgURL,
+        connect: this.data.connect,
         username: this.data.username,
-        price: this.data.price
+        price: this.data.price,
+        time: this.data.nowtime
       },
       success(res) {
         console.log("提交成功", res);
@@ -62,6 +154,8 @@ Page({
       duration: 1000
     })
   },
+
+  /*
   //添加上传图片
   chooseImageTap: function () {
     var that = this;
@@ -108,6 +202,8 @@ Page({
       fail: console.error
     })
   },
+
+  */
   //加减数量
   addNum: function () {
     var courseCount = this.data.courseCount;
@@ -136,60 +232,5 @@ Page({
     let i = e.detail.value;
     let value = this.data.selectorItems[i];
     this.setData({ selector: value });
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
