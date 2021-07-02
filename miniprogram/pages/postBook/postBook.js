b4d2002534bdf4ad03271b0db1ea4edaf326e20d
abysswatcher-hqy <2534586898@qq.com>
2021-07-02 11:27:01 +0800

    韩启宇-发布界面1.0

--- miniprogram/pages/postBook/postBook.js
+++ miniprogram/pages/postBook/postBook.js
@@ -1,7 +1,10 @@
 // pages/postBook/postBook.js
 var utils = require('../postBook/util.js');
+const db = wx.cloud.database();
 Page({
   data: {
+    bookstatus: 'notbuy',
+    again: true,
     connect:'',
     showImg: '',
     nowtime: '1',
@@ -15,7 +18,6 @@ Page({
     imgURL: [],
     courseCount: 1, //设置出售数量相关变量
     imgbox: [], //选择图片
-    picPaths: [],//网络路径
     steps: [
       {
         text: '填写图书信息'
@@ -25,6 +27,26 @@ Page({
       }
     ],
     selectorItems: ['计算机', '土木工程', '机电', '林学', '经济管理', '理学']
+  },
+
+  
+//再发一单
+  againpost: function(e) {
+    this.setData({connect:'',
+    showImg: '',
+    nowtime: '1',
+    count: 3,
+    price: 0,
+    active_step: 0,
+    fangshi: '默认',
+    username: ' ',
+    bookname: '默认',
+    minusStatus: true,
+    imgURL: [],
+    courseCount: 1, 
+    imgbox: [],
+    again: true })
+    console.log(1);
   },
 //图片上传
   // 删除照片 &&
@@ -88,47 +110,13 @@ Page({
   setconnect(e){
     this.setData({ connect: e.detail.value })
   },
-  //提交信息
-  submit() {
+  uploadAll:function() {
     this.setData({active_step: 1})
-    // 云函数入口文件
     var TIME = utils.formatTime(new Date())
     this.setData({nowtime:TIME}) 
-    const db = wx.cloud.database()
     //获取缓存中的用户名
     var value = wx.getStorageSync('user')
     this.setData({username: value.nickName})
-    //上传图片到云存储
-    console.log('总图片数量：'+this.data.imgbox.length)
-    let promiseArr = [];
-    wx.showLoading({
-      title: '图片上传中',
-      mask: true,
-    })
-    for (let i = 0; i < this.data.imgbox.length; i++) {
-      promiseArr.push(new Promise((reslove, reject) => {
-        let item = this.data.imgbox[i];
-        let suffix = /\.\w+$/.exec(item)[0]; //正则表达式返回文件的扩展名
-        wx.cloud.uploadFile({
-          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
-          filePath: item, // 小程序临时文件路径
-          success: res => {
-            this.setData({
-              fileIDs: this.data.fileIDs.concat(res.fileID),
-            });
-            console.log(this.data.fileIDs)
-            console.log(res.fileID) //输出上传后图片的返回地址
-            reslove();
-          },
-          fail: res => {
-            wx.hideLoading();
-            wx.showToast({
-              title: "上传失败",
-            })
-          }
-        })
-      }));
-    }
     db.collection('fabujilu').add({
       data: {
         //属性:变量名,
@@ -140,7 +128,8 @@ Page({
         connect: this.data.connect,
         username: this.data.username,
         price: this.data.price,
-        time: this.data.nowtime
+        time: this.data.nowtime,
+        bookstatus: this.data.bookstatus
       },
       success(res) {
         console.log("提交成功", res);
@@ -154,56 +143,45 @@ Page({
       duration: 1000
     })
   },
-
-  /*
-  //添加上传图片
-  chooseImageTap: function () {
-    var that = this;
-    wx.showActionSheet({
-      itemList: ['从相册中选择', '拍照'],
-      itemColor: "#00000",
-      success: function (res) {
-        if (!res.cancel) {
-          if (res.tapIndex == 0) {
-            that.chooseWxImage('album')
-          } else if (res.tapIndex == 1) {
-            that.chooseWxImage('camera')
+  //提交信息
+  submit() {
+    
+    //上传图片到云存储
+    console.log('总图片数量：'+this.data.imgbox.length)
+    let promiseArr = [];
+    wx.showLoading({
+      title: '图片上传中',
+      mask: true,
+    })
+    this.setData({again:false})
+    for (let i = 0; i < this.data.imgbox.length; i++) {
+      promiseArr.push(new Promise((reslove, reject) => {
+        let item = this.data.imgbox[i];
+        let suffix = /\.\w+$/.exec(item)[0]; //正则表达式返回文件的扩展名
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
+          filePath: item, // 小程序临时文件路径
+          success: res => {
+            this.setData({
+              imgURL: this.data.imgURL.concat(res.fileID)
+            });
+            console.log(this.data.imgURL)
+            console.log(res.fileID) //输出上传后图片的返回地址
+            reslove();
+          },
+          fail: res => {
+            wx.hideLoading();
+            wx.showToast({
+              title: "上传失败",
+            })
           }
-        }
-      }
-    })
-  },
-  //图片本地路径
-  chooseWxImage: function (type) {
-    var that = this;
-    var imgsPaths = that.data.imgs;
-    wx.chooseImage({
-      sizeType: ['original', 'compressed'],
-      sourceType: [type],
-      success: function (res) {
-        console.log(res.tempFilePaths[0]);
-        that.uploadImage(res.tempFilePaths[0], 0) //调用上传方法
-      }
-    })
-  },
-  //上传云存储并获取url
-  uploadImage(fileURL) {
-    wx.cloud.uploadFile({
-      cloudPath: new Date().getTime() + '.png', // 上传至云端的路径
-      filePath: fileURL, // 小程序临时文件路径
-      success: res => {
-        // 返回文件 ID
-        console.log("上传成功", res)
-        //获取文件路径
-        this.setData({
-          imgURL: res.fileID
         })
-      },
-      fail: console.error
+      }));
+    }
+    Promise.all(promiseArr).then(res => {
+      this.uploadAll();
     })
   },
-
-  */
   //加减数量
   addNum: function () {
     var courseCount = this.data.courseCount;
