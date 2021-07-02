2cd89a3073a7d1c9667d856e149b9fa5f77534db
小绵杨 <3329929364@qq.com>
2021-07-02 21:25:43 +0800

    张杨、韩启宇

--- miniprogram/pages/postBook/postBook.js
+++ miniprogram/pages/postBook/postBook.js
@@ -18,6 +18,7 @@ Page({
     imgURL: [],
     courseCount: 1, //设置出售数量相关变量
     imgbox: [], //选择图片
+    picPaths: [],//网络路径
     steps: [
       {
         text: '填写图书信息'
@@ -27,26 +28,6 @@ Page({
       }
     ],
     selectorItems: ['计算机', '土木工程', '机电', '林学', '经济管理', '理学']
-  },
-
-  
-//再发一单
-  againpost: function(e) {
-    this.setData({connect:'',
-    showImg: '',
-    nowtime: '1',
-    count: 3,
-    price: 0,
-    active_step: 0,
-    fangshi: '默认',
-    username: ' ',
-    bookname: '默认',
-    minusStatus: true,
-    imgURL: [],
-    courseCount: 1, 
-    imgbox: [],
-    again: true })
-    console.log(1);
   },
 //图片上传
   // 删除照片 &&
@@ -110,13 +91,47 @@ Page({
   setconnect(e){
     this.setData({ connect: e.detail.value })
   },
-  uploadAll:function() {
+  //提交信息
+  submit() {
     this.setData({active_step: 1})
+    // 云函数入口文件
     var TIME = utils.formatTime(new Date())
     this.setData({nowtime:TIME}) 
+    const db = wx.cloud.database()
     //获取缓存中的用户名
     var value = wx.getStorageSync('user')
     this.setData({username: value.nickName})
+    //上传图片到云存储
+    console.log('总图片数量：'+this.data.imgbox.length)
+    let promiseArr = [];
+    wx.showLoading({
+      title: '图片上传中',
+      mask: true,
+    })
+    for (let i = 0; i < this.data.imgbox.length; i++) {
+      promiseArr.push(new Promise((reslove, reject) => {
+        let item = this.data.imgbox[i];
+        let suffix = /\.\w+$/.exec(item)[0]; //正则表达式返回文件的扩展名
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
+          filePath: item, // 小程序临时文件路径
+          success: res => {
+            this.setData({
+              fileIDs: this.data.fileIDs.concat(res.fileID),
+            });
+            console.log(this.data.fileIDs)
+            console.log(res.fileID) //输出上传后图片的返回地址
+            reslove();
+          },
+          fail: res => {
+            wx.hideLoading();
+            wx.showToast({
+              title: "上传失败",
+            })
+          }
+        })
+      }));
+    }
     db.collection('fabujilu').add({
       data: {
         //属性:变量名,
@@ -141,6 +156,25 @@ Page({
     wx.showToast({
       title: '发布成功',
       duration: 1000
+    })
+  },
+
+  /*
+  //添加上传图片
+  chooseImageTap: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#00000",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
     })
   },
   //提交信息
@@ -182,6 +216,8 @@ Page({
       this.uploadAll();
     })
   },
+
+  */
   //加减数量
   addNum: function () {
     var courseCount = this.data.courseCount;
